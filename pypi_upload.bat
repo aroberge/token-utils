57df0089efa704f09e23eae02d83a6ed64e7348a
André Roberge <andre.roberge@gmail.com>
2020-02-26 11:06:31 -0400

    Keeping version in sync with that of ideas

--- pypi_upload.bat
+++ pypi_upload.bat
@@ -1,4 +1,8 @@
 echo off
+cp ..\ideas\ideas\token_utils.py token_utils\token_utils.py
+git add *
+git commit -m "Keeping version in sync with that of ideas"
+git push
 :Ask
 echo Did you update the version?(y/n)
 set ANSWER=
